85e67576af2e1c6e9bf67bbd5b3738cca4449993
MerimaMuhovic <merima.muhovic@hotmail.com>
2023-04-09 02:05:07 +0200

    initial push

---
+++ F1TireManagement/F1TireManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1TireManagement
+//
+//  Created by Merima Muhovic on 9. 4. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
